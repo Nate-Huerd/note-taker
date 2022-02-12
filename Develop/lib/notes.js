eb3dcef9a90b4497eded2eaaa91e273bb6bc6088
Nate-Huerd <nhuerd3@gmail.com>
2022-02-12 14:53:03 -0600

    removed eureka folder as it was not showing code in GitHub

---
+++ Develop/lib/notes.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+// must declare path for directory to work
+const path = require('path');
+
+// function to create a new note
+function createNewNote(body, notes) {
+    const note = body;
+    // need to push the new note in order to GET it later
+    notes.push(note);
+    
+    // write file, use JSON.stringify
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify({notes}, null, 2)
+    )
+    // return the note created
+    return notes;
+};
+
+// export createNewNote
+module.exports = {createNewNote};
