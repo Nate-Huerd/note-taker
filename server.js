dfd8ce86f26d33b2d152c4cf435dce7a90066bfc
Nate-Huerd <nhuerd3@gmail.com>
2022-02-13 16:47:11 -0600

    renamed noteRoutes file to notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // refer to module 11 for beginning a server.js file
 const express = require('express');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
-// express app.use statements (refer to documentation)
+// Middleware express app.use statements (refer to documentation)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
